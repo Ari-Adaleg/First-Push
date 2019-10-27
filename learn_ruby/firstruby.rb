d3a1fb7e0eb48ef08a5ea9c4f4120bfdf5ab2196
Ari Drookman <aga.drookman@gmail.com>
2019-10-27 09:08:00 -0400

    First time ruby

---
+++ learn_ruby/firstruby.rb
@@ -0,0 +1,12 @@
+puts "Welcome!
+What's your name?"
+name = gets.chomp
+puts "Hello, #{name}! Nice to meet you!"
+puts "How old are you?"
+age = gets.chomp
+puts "Wow! I didn't know that you are #{age} years old."
+puts "Now that I know you a little bit, I want to come and visit you in person!
+Where do you live, #{name}?"
+live = gets.chomp
+puts "\n\nOh... #{live} is so far from here in my computer... Maybe I'll come another time.
+Goodbye for now, #{name}!"
